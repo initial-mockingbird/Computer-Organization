3e7afa3112372c8012d9985d95978169979aa2ae
xdgorgola <pedro18fe@gmail.com>
2020-03-09 21:56:28 -0400

    Lista popeando el fracando y pushando al exito

--- Proyecto2/Lista/list_crear.asm
+++ Proyecto2/Lista/list_crear.asm
@@ -1,8 +1,6 @@
 ## --- Plan list_crear --- ##
 #
-# In Params:
-#	$a0: List elements comparison method address
-#	$a1: List elements print method address
+# In Params: <NONE>
 #
 # Out Params:
 #	$v0: Operation ending code (-1 faulty execution/0 successfull execution)
@@ -20,16 +18,9 @@
 # PRIMERO LLEVA UNA CABEZA QUE CONTIENE:
 #
 #	~Direccion header
-#	~Direccion funcion comparacion
-#	~Direccion funcion impresion
+#	~Direccion cola/ultimo elemento
 #	~Direccion Primer elemento
-#
-# CADA ELEMENTO DE LA LISTA ES UN STRUCT TAL QUE:
-#
-#	~Direccion actual en memoria
-#	~Direccion Valor
-#	~Direccion previo
-#	~Direccion siguiente
+
 #
 list_crear:
 	# EPILOGUE
@@ -37,31 +28,18 @@ list_crear:
 	move	$fp, $sp
 	addiu	$sp, $sp, -4
 	
-	move	$t0, $a0 # We temporaly store comparison method address to allocate memory
-	
 	li	$v0, 9 # We allocate list HEADER space
 	li	$a0, 16
 	syscall
 	
-	move	$v1, $v0 # We store list address in return register
-	
-	beqz	$v0, mem_unavailable # We check if the returned dir is valid
-	
 	sw	$v0, 0($v0) # We store HEADER address
-	sw	$t0, 4($v0) # We store list comparison method
-	sw	$a1, 8($v0) # We store list print method
-	
+	sw	$zero, 4($v0) # We store list size
 	li	$t0, 0xffffffff
-	sw	$t0, 12($v0) # We define HEADER next element as NIL
+	sw	$t0, 8($v0) # We store list TAIL/LAST ELEMENT dir
+	sw	$t0, 12($v0) # We define HEADER next element as NIL dir
 
-	li	$v0, 0
 	j	list_crear_exit
 	
-
-mem_unavailable:
-	li	$v0, -1
-	j	list_crear_exit
-
 
 list_crear_exit:
 	# PROLOGUE
--- Proyecto2/Lista/list_insertar.asm
+++ Proyecto2/Lista/list_insertar.asm
@@ -1,22 +1,21 @@
-##### Double-Linked List Insert Function #####
+##### Double-Linked List Push Function #####
 #
 # In Params:
 #	$a0: List address (Header address)
 #	$a1: Element to insert address
 #
-# Out Params:
-#	$v0: Operation ending code (-1 faulty execution/0 successfull execution)
+# Out Params: <NONE>
 #
 # Side Effects:
 #	List located in $a0 address now has element $a1 inserted as its
-#	final element.
+#	first element
 # 
 # Method variables:
-#	$a0: Current element address
-#	$t0: Comparison method for the elements
-#	$t1: Previous element in list
+#	$v0: toInsert NODE address
+#	$t1: HEADER address
+#	$t2: Previous FIRST address
 	
-list_insertar:
+list_push:
 	# PROLOGUE
 	sw	$fp, 0($sp)
 	move	$fp, $sp
@@ -24,74 +23,44 @@ list_insertar:
 	sw	$ra, 4($sp)
 	
 	move	$t1, $a0 # First we store the header address
-	lw	$t0, 4($a0) # We store the list element comparison method
 
-	lw	$a0, 12($a0) # We load the next position address
-	beq	$a0, 0xffffffff, last_element # Checks if the list if empty
-	j	compare_loop
+	lw	$t2, 12($a0) # We load the next position address
+	j	insert_first
 	
 	
-compare_loop:
-	# STORING DATA JUST IN CASE
-	addi	$sp, $sp, -16
-	sw	$a0, 16($sp)
-	sw	$a1, 12($sp)
-	sw	$t0, 8($sp)
-	sw	$t1, 4($sp)
-	
-	lw	$a0, 4($a0) # We load current list element value
-	jalr	$t0 # We compare $a0 and $a1 (current list and element to insert)
-	
-	# RESTORING DATA
-	lw	$a0, 16($sp)
-	lw	$a1, 12($sp)
-	lw	$t0, 8($sp)
-	lw	$t1, 4($sp)
-	addi	$sp, $sp, 16
-	
-	bgez	$v0, insert_here # If to_insert <= current_element
-	
-	move	$t1, $a0 # We update previous element address to the current element
-	lw	$a0, 12($a0) # We load next element address
-	beq	$a0, 0xffffffff, last_element # If there is no next element, add
-	j	compare_loop
-		
-insert_here:
-	move	$t3, $a0 # We prevent $a0 value loss
-	
-	li	$v0, 9 # We allocate new element memory
-	li	$a0, 16
-	syscall
-	
-	sw	$v0, 12($t1) # The previous next goest to new element
-	sw	$v0, 0($v0) # We store new element address
-	sw	$a1, 4($v0) # We store new element value address
-	sw	$t1, 8($v0) # The new prev goes to the previous
-	sw	$t3, 12($v0) # The new next goest to current element
-	
-	sw	$v0, 8($t3) # The current previous goes to new
-	
-	li	$v0, 0
-	j	list_insert_exit
-	
-	
-last_element:
+insert_first:
 	li	$v0, 9
 	li	$a0, 16
 	syscall
 	
-	sw	$v0, 0($v0) # The
-	sw	$a1, 4($v0) # The new value
-	sw	$t1, 8($v0) # The next previous goes to the previous
-	li	$a0, 0xffffffff
-	sw	$a0, 12($v0) # The new next goes to NIL
+	sw	$v0, 0($v0) # The list NODE address
+	sw	$a1, 4($v0) # The toInsert is inserted in the NODE
+	sw	$t1, 8($v0) # The toInsert previus goes to HEADER
 	
-	sw	$v0, 12($t1) # The previous next goes to new element
+	beq	$t2, -1, empty_insert
 	
-	li	$v0, 0
-	j	list_insert_exit
+	sw	$t2, 12($v0) # The toInsert next goes to the fist element in list 
 	
-list_insert_exit:
+	sw	$v0, 8($t2) # The nextElement previous goes to toInsert NODE address
+	
+	j	update_first
+
+
+empty_insert:
+	li	$a0, -1
+	sw	$a0, 12($v0)	# The toInsert next goes to NIL	
+	sw	$v0, 8($t1)	# The header tail goes to first
+	
+	
+update_first:
+	sw	$v0, 12($t1)	# THe HEADER first goes to the toInsert node
+	# We now update list size
+	lw	$a0, 4($t1)
+	addi	$a0, $a0, 1
+	sw	$a0, 4($t1)
+	
+	
+list_push_exit:
 	# EPILOGUE
 	lw	$ra, 4($sp)
 	move	$sp, $fp
